944d7dfc95fda17b495cb6aa616b151f5bcb2936
adenijiamir-wq <adenijiamir@gmail.com>
2026-03-08 15:36:54 -0400

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window._firebaseConfig = {
+  apiKey: "AIzaSyAmreqeMafYfB38tQoe24QboOFCh4BzReA",
+  authDomain: "flow-note-dc460.firebaseapp.com",
+  projectId: "flow-note-dc460",
+  storageBucket: "flow-note-dc460.firebasestorage.app",
+  messagingSenderId: "952497380530",
+  appId: "1:952497380530:web:ba126716f67db5a30ffdab"
+};
