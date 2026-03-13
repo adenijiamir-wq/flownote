ca572a96de67683d0f770720da0ee3c2c9342b50
adenijiamir-wq <adenijiamir@gmail.com>
2026-03-13 06:54:18 -0400

    send-email.js

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -47,7 +47,7 @@ exports.handler = async function(event) {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        sender: { name: 'Menmory', email: 'noreply@menmory.netlify.app' },
+        sender: { name: 'Menmory', email: 'adenijiamir@gmail.com' },
         to: [{ email: to_email }],
         subject: subject,
         htmlContent: message_html
